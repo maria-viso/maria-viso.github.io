4a74244fd19860ff63e16ff8ff8257a7349e4312
María Viso <mariaviso@outlook.com>
2018-03-12 18:49:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Reticulas template
+
+Simple static site generator
+
+## Instalation
+- Download or clone master branch
+- Run npm install
+
+## Run
+
+Run on root folder:
+
+```
+npm run watch
+```
+
+```
+npm run production
+```
---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+import $ from 'jquery'
+
+$(function(){
+    var mq = window.matchMedia( "(min-width: 420px)" );
+    // Parallax effect
+    var parallax = function($object, time){
+        time = time++;
+        $(window).scroll(function(){
+            var scrolling = $(window).scrollTop();
+            var move = scrolling * time + 'px';
+            $object.css({ 'margin-top' : move });
+        });
+    }
+    //parallax($(".description"), -.20);
+    if (mq.matches) {
+        parallax($(".projects li:nth-child(2)"), .15);
+        parallax($(".projects li:nth-child(7)"), -.25);
+        parallax($(".projects li:nth-child(12)"), -.10);
+    }
+});
